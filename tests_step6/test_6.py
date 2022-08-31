8b5cde7eb607a000a2762c1ec26fcb0a01dc4b62
Kalinovsky Konstantin <norraxx@gmail.com>
2022-08-31 12:22:32 +0300

    Update test_6.py
    Lost file

--- tests_step6/test_6.py
+++ tests_step6/test_6.py
@@ -1,2 +1,58 @@
-def test():
+import pytest
+
+from some_evil_code import foo, Foo
+from mock import patch, Mock
+import time
+
+
+def test_mock_object(fake_target_bar, mocker):
+    with fake_target_bar(foo):
+        assert foo.bar() == 20
+    assert foo.bar() == 10
+
+    with patch.object(foo, "bar", return_value=20):
+        assert foo.bar() == 20
+
+    assert foo.bar() == 10
+
+    with mocker.patch.object(foo, "bar", return_value=20):
+        assert foo.bar() == 20
+
+    assert foo.bar() == 20  # ]:->
+
+    assert False
+
+
+def test_mock_class_method(mocker):
+    with patch.object(Foo, "bar", return_value=20) as mock:
+        assert Foo().bar() == 20
+    mock.assert_called()
+
+    assert Foo().bar() == 10
+
+    with mocker.patch.object(Foo, "bar", return_value=20):
+        assert Foo().bar() == 20
+
+    assert Foo().bar() == 20
+
+    assert False
+
+
+def test_mock_function(monkeypatch):
+    monkeypatch.setattr(time, "time", lambda: 0)
+    assert time.time() == 0
+
+    with patch.object(Foo, "bar", return_value=20) as mock:
+        assert Foo().bar() == 20
+    mock.assert_called()
+
+    assert False
+
+
+def test_mock_as_exception():
+    with pytest.raises(Exception):
+        mock = Mock(side_effect=Exception("Python rocks!"))
+        mock()
+    mock.assert_called()
+
     assert False
